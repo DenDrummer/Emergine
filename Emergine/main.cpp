49009b5f8a2fc8f32ebca9806c4d9e9946ddff38
DenDrummer <muzikant96@gmail.com>
2021-04-06 17:52:08 +0200

    General structure

--- Emergine/main.cpp
+++ Emergine/main.cpp
@@ -1,3 +1,49 @@
-int main() {
+#include <vulkan/vulkan.h>
 
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+class EmergineApp {
+public:
+	void run() {
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	#pragma region --- INIT VULKAN ---
+	void initVulkan() {
+
+	}
+	#pragma endregion INIT VULKAN
+	
+	#pragma region --- MAIN LOOP ---
+	void mainLoop() {
+
+	}
+	#pragma endregion MAIN LOOP
+	
+	#pragma region --- CLEANUP ---
+	void cleanup() {
+
+	}
+	#pragma endregion CLEANUP
+};
+
+int main() {
+	EmergineApp app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
