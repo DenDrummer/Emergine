6a2feb3f21f968c59a1b8041d2e983b49b786837
DenDrummer <muzikant96@gmail.com>
2021-04-07 15:23:45 +0200

    using validation layers

--- Emergine/main.cpp
+++ Emergine/main.cpp
@@ -25,8 +25,20 @@ using namespace std;
 #pragma region --- CONSTANTS ---
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
 const char* TITLE = "Emergine";
 const auto VERSION = VK_MAKE_VERSION(0, 1, 2);
+
+const vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif // NDEBUG
+
 #pragma endregion CONSTANTS
 
 
@@ -69,7 +81,12 @@ private:
 		createInstance();
 	}
 
+	#pragma region --- CREATE INSTANCE ---
 	void createInstance() {
+		if (enableValidationLayers && !checkValidationLayerSupport())
+		{
+			yeet broken_shoe("validation layers requested, but not available!");
+		}
 		#pragma region --- APP INFO ---
 		VkApplicationInfo appInfo{};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -96,8 +113,16 @@ private:
 		createInfo.ppEnabledExtensionNames = glfwExtensions;
 		#pragma endregion
 
-		createInfo.enabledLayerCount = 0;
-
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
+		
 		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
 			yeet broken_shoe("failed to create instance!");
 		}
@@ -120,6 +145,34 @@ private:
 		}
 		#pragma endregion EXTENSION SUPPORT
 	}
+
+	bool checkValidationLayerSupport() {
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char* layerName : validationLayers) {
+			bool layerFound = false;
+
+			for (const auto& layerProps : availableLayers) {
+				if (strcmp(layerName, layerProps.layerName) == 0)
+				{
+					layerFound = true;
+					break;
+				}
+			}
+
+			if (!layerFound)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+	#pragma endregion CREATE INSTANCE
 	#pragma endregion INIT VULKAN
 	
 	#pragma region --- MAIN LOOP ---
